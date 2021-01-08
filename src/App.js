d05e5b4d2e92c2e8d3c85a6427bead9f16cfec12
MAussem <Aussem.Maxwell@gmail.com(home)>
2021-01-07 20:23:46 -0500

    created a mock local storage app

--- src/App.js
+++ src/App.js
@@ -1,30 +1,45 @@
 import React from 'react';
-import DeviceOrientation, { Orientation } from 'react-screen-orientation'
 
+// setter
+// localStorage.setItem('myData', data);
 
+// getter
+// localStorage.getItem('myData');
 
-function App() {
+// remove
+// localStorage.removeItem('myData');
+
+// remove all
+// localStorage.clear();
+
+const App = () => {
+
+  // to manage state of our input field
+  const [value, setValue] = React.useState(
+    // Retrieves the stored value of state
+    localStorage.getItem('myValueInLocalStorage') || '');
+
+  // runs everytime value property is changed (state change)
+  React.useEffect(() => {
+    // Stores the recent value (state)
+    localStorage.setItem('myValueInLocalStorage', value);
+  }, [value]);
+
+  // set state to input value onChange
+  const onChange = event => setValue(event.target.value);
 
   return (
 
     <div>
-      <DeviceOrientation lockOrientation={'portrait'}>
-        {/* Will only be in DOM in landscape */}
-        <Orientation orientation='portrait' alwaysRender={false}>
-          <div>
-            <p>Only visible in landscape</p>
-          </div>
-        </Orientation>
-        {/* Will stay in DOM, but is only visible in portrait */}
-        <Orientation orientation='landscape'>
-          <div>
-            <p>Please rotate your device</p>
-          </div>
-        </Orientation>
-      </DeviceOrientation>
-    </div>
 
+      <h1>Max's Local Storage Test!</h1>
+      {/* state value starts empty, onChange setValue takes input value, and changes to new state */}
+      <input value={value} type="text" onChange={onChange} />
+      {/* result of new state shown as output in p tag */}
+      <p>{value}</p>
+
+    </div>
   );
-}
+};
 
 export default App;
